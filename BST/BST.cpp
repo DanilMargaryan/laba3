99ec3c1fb763fb5f1cfaae3502e36a028a8bb84b
Adam <59186293+AdamS1604@users.noreply.github.com>
2021-04-27 01:29:10 +0300

    First commit

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,7 +1,33 @@
 #include "BST.h"
 #include <iostream>
 
-Node::Node(int Key)
+Node::Node(int key)
 {
-    this->Key = Key;
+    this->key = key;
+    this->left = nullptr;
+    this->right = nullptr;
+}
+
+BST::BST()
+{
+    this->root = nullptr;
+}
+
+void BST::add(int key)
+{
+    Node n(key);
+    Node* ptr;
+
+    ptr = this->root;
+    if (this->root == nullptr)
+        this->root = &n;
+    else
+    {
+        while (ptr != nullptr)
+            if (n.key <= ptr->key)
+                ptr = ptr->left;
+            else 
+                ptr = ptr->right;
+        Node   
+    }
 }
